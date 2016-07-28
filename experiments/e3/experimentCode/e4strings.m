a9ba32a927f212ba3e06b1ceb4c4a150dc131511
matt_green <mjglab@gmail.com>
2016-07-28 17:41:10 +0100

    added e3, but lots of its files and contents need renaming from exp4 to e3 to be consistent with the 2016 numbering

---
+++ experiments/e3/experimentCode/e4strings.m
@@ -0,0 +1,11 @@
+    %%%%%%%%%%%%%%%%%%%%%%%
+    % define text strings %
+    %%%%%%%%%%%%%%%%%%%%%%%
+
+    instructionText = sprintf(strcat...
+         ('Press Enter to start the practice trials.'));
+
+    oopsText = sprintf(strcat(...
+        'Ooops... Something went wrong.\n\n',...
+        'Please tell the experimenter.'));
+
