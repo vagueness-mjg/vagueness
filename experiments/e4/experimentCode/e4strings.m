4aa11a30fc34cc74bc045dfbd11e2f9f9628646e
matt_green <mjglab@gmail.com>
2016-07-28 20:18:56 +0100

    adding e4

---
+++ experiments/e4/experimentCode/e4strings.m
@@ -0,0 +1,10 @@
+    %%%%%%%%%%%%%%%%%%%%%%%
+    % define text strings %
+    %%%%%%%%%%%%%%%%%%%%%%%
+
+    instructionText = sprintf(strcat('Press Enter to start the practice trials.'));
+
+    oopsText = sprintf(strcat(...
+        'Ooops... Something went wrong.\n\n',...
+        'Please tell the experimenter.'));
+
