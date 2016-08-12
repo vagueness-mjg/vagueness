e4b0edf026cce03cae439cb369ef44ba8d715771
Matt Green <mjglab@gmail.com>
2016-08-12 22:34:02 +0100

    more consistency, better plots, using discriminability instead of item ratio

---
+++ experiments/e2/writeup/Untitled1.R
@@ -0,0 +1,7 @@
+scale_x_continuous(minor_breaks=NULL,
+                   breaks=c(0.5125000, 0.6876471, 0.7691558, 0.8166667),
+                   labels=abbreviate(c(0.5125000, 0.6876471, 0.7691558, 0.8166667))
+                   
+scale_x_continuous(minor_breaks=NULL,
+                  breaks=1-c(0.5125000, 0.6876471, 0.7691558, 0.8166667),
+                  labels=abbreviate(1-c(0.5125000, 0.6876471, 0.7691558, 0.8166667))
