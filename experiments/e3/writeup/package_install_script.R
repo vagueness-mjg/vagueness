05aa9a7186c192c4dc0910baa2d9c841bdbe21d7
Matt Green <mjglab@gmail.com>
2017-05-16 15:33:04 +0100

    ggplot computes summary on the fly using mean_cl_boot

---
+++ experiments/e3/writeup/package_install_script.R
@@ -0,0 +1,2 @@
+install.packages(c("knitr", "MASS", "ggplot2", "lme4", "xtable", "plyr", "lattice", "LMERConvenienceFunctions", "car", 
+                   "lmerTest", "reshape2", "ggplot2", "languageR", "pander"))
