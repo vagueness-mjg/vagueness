3d45ed755c1cdb6ae95d9d092e5a414352a2d235
Matt Green <mgreen1718@gmail.com>
2018-08-03 12:34:55 +0100

    add add stars()

---
+++ experiments/C_exp_1/writeup/add_stars.R
@@ -0,0 +1,45 @@
+add_stars = function(modl) {
+  
+  coef_table <- tidy(modl, effects="fixed") 
+  
+  # be sensitive to whether the model is an lmer or a glmer 
+  
+  if (class(modl) == "lmerModLmerTest") {
+    
+    # sort the names out
+    names(coef_table) <- c("effect", "term", "coefficient", "std.error", "df", "t_value", "p_value_raw")
+    coef_table$p_value <- as.character("")
+    coef_table$star <- as.character("")
+    
+    # categorise p values and add significance stars
+    # "\u2007" is a space the same width as a numeric, that is preserved in printing
+    # "\u0020" is just a space
+    for (i in 1:nrow(coef_table)) {
+      if (!is.na(coef_table$p_value_raw[i])) {
+        if (coef_table$p_value_raw[i] >= 0     & coef_table$p_value[i] < 0.001) {coef_table$p_value[i] <-    "<0.001"; coef_table$star[i] <- "***"}
+        if (coef_table$p_value_raw[i] >= 0.001 & coef_table$p_value[i] < 0.01 ) {coef_table$p_value[i] <-    "<0.01"; coef_table$star[i] <- "**"}
+        if (coef_table$p_value_raw[i] >= 0.01  & coef_table$p_value[i] < 0.05 ) {coef_table$p_value[i] <-    "<0.05"; coef_table$star[i] <- "*"}
+        if (coef_table$p_value_raw[i] >= 0.05  ) {coef_table$p_value[i] <-    round(coef_table$p_value_raw[i],3); coef_table$star[i] <-  ""}
+      }
+    }
+    
+    # un-tibble the table
+    coef_table <- coef_table %>% as.data.frame()
+    
+    # convert numerics to formatted chars for pretty printing
+    coef_table$coefficient <- format(x=coef_table$coefficient, nsmall=2, digits=2)
+    coef_table$std.error   <- format(x=coef_table$std.error, nsmall=2, digits=2)
+    coef_table$df          <- format(x=coef_table$df, nsmall=2, digits=1)
+    coef_table$t_value     <- format(x=coef_table$t_value, nsmall=2, digits=2)
+    coef_table$p_value_raw <- format(x=coef_table$p_value_raw, nsmall=2, digits=3)
+    
+    # select rows and cols
+    coef_table <- coef_table %>%
+      select(-effect) %>%
+      filter(term!="(Intercept)") 
+    
+    return(coef_table)
+  
+    }
+  
+}
