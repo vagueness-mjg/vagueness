2b9e6adca8a6ce1a8a81bd1c1d163af5f588548a
Matt Green <mgreen1718@gmail.com>
2018-08-11 11:15:39 +0100

    minor edit

--- experiments/C_exp_1/writeup/add_stars.R
+++ experiments/C_exp_1/writeup/add_stars.R
@@ -1,6 +1,6 @@
 add_stars = function(modl) {
   
-  coef_table <- tidy(modl, effects="fixed") 
+  coef_table <- as.data.frame(tidy(modl, effects="fixed"))
   
   # be sensitive to whether the model is an lmer or a glmer 
   
@@ -12,8 +12,6 @@ add_stars = function(modl) {
     coef_table$star <- as.character("")
     
     # categorise p values and add significance stars
-    # "\u2007" is a space the same width as a numeric, that is preserved in printing
-    # "\u0020" is just a space
     for (i in 1:nrow(coef_table)) {
       if (!is.na(coef_table$p_value_raw[i])) {
         if (coef_table$p_value_raw[i] >= 0     & coef_table$p_value[i] < 0.001) {coef_table$p_value[i] <-    "<0.001"; coef_table$star[i] <- "***"}
@@ -22,10 +20,7 @@ add_stars = function(modl) {
         if (coef_table$p_value_raw[i] >= 0.05  ) {coef_table$p_value[i] <-    round(coef_table$p_value_raw[i],3); coef_table$star[i] <-  ""}
       }
     }
-    
-    # un-tibble the table
-    coef_table <- coef_table %>% as.data.frame()
-    
+
     # convert numerics to formatted chars for pretty printing
     coef_table$coefficient <- format(x=coef_table$coefficient, nsmall=2, digits=2)
     coef_table$std.error   <- format(x=coef_table$std.error, nsmall=2, digits=2)
@@ -40,6 +35,41 @@ add_stars = function(modl) {
     
     return(coef_table)
   
+  }
+  
+  if (class(modl) == "glmerMod") {
+    
+    # sort the names out
+    names(coef_table) <- c("effect", "term", "coefficient", "std.error", "z_value", "p_value_raw")
+    coef_table$p_value <- as.character("")
+    coef_table$star <- as.character("")
+    
+    # categorise p values and add significance stars
+    for (i in 1:nrow(coef_table)) {
+      if (!is.na(coef_table$p_value_raw[i])) {
+        if (coef_table$p_value_raw[i] >= 0     & coef_table$p_value[i] < 0.001) {coef_table$p_value[i] <-    "<0.001"; coef_table$star[i] <- "***"}
+        if (coef_table$p_value_raw[i] >= 0.001 & coef_table$p_value[i] < 0.01 ) {coef_table$p_value[i] <-    "<0.01"; coef_table$star[i] <- "**"}
+        if (coef_table$p_value_raw[i] >= 0.01  & coef_table$p_value[i] < 0.05 ) {coef_table$p_value[i] <-    "<0.05"; coef_table$star[i] <- "*"}
+        if (coef_table$p_value_raw[i] >= 0.05  ) {coef_table$p_value[i] <-    round(coef_table$p_value_raw[i],3); coef_table$star[i] <-  ""}
+      }
     }
+    
+    # convert numerics to formatted chars for pretty printing
+    coef_table$coefficient <- format(x=coef_table$coefficient, nsmall=2, digits=2)
+    coef_table$std.error   <- format(x=coef_table$std.error, nsmall=2, digits=2)
+    coef_table$df          <- format(x=coef_table$df, nsmall=2, digits=1)
+    coef_table$z_value     <- format(x=coef_table$z_value, nsmall=2, digits=2)
+    coef_table$p_value_raw <- format(x=coef_table$p_value_raw, nsmall=2, digits=3)
+
+    
+    # kable styling etc
+    coef_table <- coef_table %>%
+      select(-effect) %>%
+      filter(term!="(Intercept)") %>%
+      kable(align='rrlrrrl', caption=mycaption) %>% 
+      kable_styling(full_width = F, position = "left", font_size = 11)
+    
+    return(coef_table)
+  }
   
 }
